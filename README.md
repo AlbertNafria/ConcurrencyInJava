4d9f5b00c7f4a000938e40749384f5ff07dfa48c
Albert Nafría <albert.nafria@gmail.com>
2022-03-25 18:03:33 +0100

    Fair locks

---
+++ README.md
@@ -0,0 +1,4 @@
+# Concurrency in Java
+
+Code written while following a course on Java concurrency, playing with threads
+and processes.
--- src/dev/albertnafria/starvation/Starvation.java
+++ src/dev/albertnafria/starvation/Starvation.java
@@ -1,8 +1,11 @@
 package dev.albertnafria.starvation;
 
 import dev.albertnafria.utils.ThreadColor;
+
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Starvation {
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true); //parameter of true is whether is a fair lock
 
     public static void main(String[] args) {
 	// write your code here
@@ -12,11 +15,11 @@ public class Starvation {
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 2");
 
-        //t1.setPriority(10);
-        //t2.setPriority(8);
-        //t3.setPriority(6);
-        //t4.setPriority(4);
-        //t5.setPriority(2);
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
 
         t2.start();
         t5.start();
@@ -36,10 +39,12 @@ public class Starvation {
         @Override
         public void run() {
             for(int i=0; i<100; i++) {
-                Thread t1 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 10");
-                synchronized (lock){
+                lock.lock();
+                try {
                     System.out.format(threadColor + "%s: runCount = %d\n", Thread.currentThread().getName(), runCount++);
                     // executes critical section of code
+                } finally {
+                    lock.unlock();
                 }
             }
 
